25a6561e9158e9e46c65e904d616e4c27baf59fd
Jiri Kalmar <kalmar.jiri@gmail.com>
2024-04-11 00:07:07 +0200

    Complete functionality, add data saving upon reload

--- script.js
+++ script.js
@@ -1,61 +1,49 @@
 'use strict';
 
 const inputElement = document.querySelector('input');
-const btnLog = document.querySelector('.btn-log');
+const btnAdd = document.querySelector('.btn-add');
 const btnsDelete = document.querySelectorAll('.btn-delete');
 const taskList = document.querySelector('ul');
 
 const createTask = () => {
   const taskText = inputElement.value.trim();
   if (taskText !== '') {
-    const taskDiv = document.createElement('div');
-    taskDiv.classList.add('task');
-
-    const taskLi = document.createElement('li');
-    taskLi.textContent = taskText;
-
-    const btnDiv = document.createElement('div');
-    btnDiv.classList.add('task-btn');
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('btn-delete');
-    deleteBtn.textContent = '❌';
-
-    btnDiv.appendChild(deleteBtn);
-    taskDiv.appendChild(taskLi);
-    taskDiv.appendChild(btnDiv);
-
-    taskList.appendChild(taskDiv);
-
-    taskDiv.querySelector('.btn-delete').addEventListener('click', removeTask);
+    const li = document.createElement('li');
+    li.textContent = taskText;
+    taskList.appendChild(li);
+    const span = document.createElement('span');
+    span.textContent = '\u00d7';
+    li.appendChild(span);
 
     inputElement.value = '';
+    saveData();
   }
 };
 
-const removeTask = e => {
-  e.target.parentElement.parentElement.remove();
-};
-
-btnLog.addEventListener('click', createTask);
+// Adding new task
+btnAdd.addEventListener('click', createTask);
 document.addEventListener('keydown', e => {
   if (e.key === 'Enter') {
     createTask();
   }
 });
 
-btnsDelete.forEach(button => {
-  button.addEventListener('click', removeTask);
+// Clicking on the task element - either COMPLETE or REMOVE (depeding on the position of click)
+taskList.addEventListener('click', e => {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+    saveData();
+  } else if (e.target.tagName === 'SPAN') {
+    e.target.parentElement.remove();
+    saveData();
+  }
 });
 
-// // Another way of deleting tasks
-// const removeTask = event => {
-//   const taskDiv = event.target.closest('.task');
-//   if (taskDiv) {
-//     taskDiv.remove();
-//   }
-// };
+const saveData = () => {
+  localStorage.setItem('data', taskList.innerHTML);
+};
 
-// btnsDelete.forEach(button => {
-//   button.addEventListener('click', removeTask);
-// });
+const showTask = () => {
+  taskList.innerHTML = localStorage.getItem('data');
+};
+showTask();
